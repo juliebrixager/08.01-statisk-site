538ebc29d9f4a0d1662ee7671b2685ca15bc2278
Julie Brixager <jubr0001@stud.ek.dk>
2026-03-03 09:15:23 +0100

    ikke noget der skal bruges

--- js/produkt.js
+++ js/produkt.js
@@ -1,35 +1,39 @@
 const params = new URLSearchParams(window.location.search);
-
 const id = params.get("id");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/${product.id}`).then((response) => response.json(),).then
+fetch(`https://kea-alt-del.dk/t7/api/products/${id}`)
+  .then((response) => response.json())
+  .then((product) => {
+    showProduct(product);
+  });
 
-const productContainer = document.querySelector(".produkt_container");
+function showProduct(product) {
+  const productContainer = document.querySelector(".produkt_container");
 
-productContainer.innerHTML = `<article class="product_image">
-        <img src="https://kea-alt-del.dk/t7/images/webp/640/1529.webp" alt="" />
-      </article>
-      <article class="productinformation">
-        <h2>Product information</h2>
-        <h3>Model name</h3>
-        <p>Red t-shirt Ferrari</p>
-        <h3>Color</h3>
-        <p>Red</p>
-        <h1>Ferrari</h1>
-        <p>Ferrari, we are the competetion</p>
-      </article>
-      <article class="size_and_purchase">
-        <h2>Ferrarai cotton t-shirt red</h2>
-        <p>Ferrari / t-shirts</p>
-        <div class="size-row">
-          <label for="size">Choose a size</label>
-
-          <select id="size">
-            <option>S</option>
-            <option>M</option>
-            <option>L</option>
-            <option>XL</option>
-          </select>
-        </div>
-        <button class="basket-btn">Add to basket</button>
-      </article>`;
+  productContainer.innerHTML = `
+    <article class="product_image">
+      <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="${product.productdisplayname}" />
+    </article>
+    <article class="productinformation">
+      <h2>Product information</h2>
+      <h3>Model name</h3>
+      <p>${product.productdisplayname}</p>
+      <h3>Color</h3>
+      <p>${product.articletype}</p>
+      <h1>${product.brandname}</h1>
+    </article>
+    <article class="size_and_purchase">
+      <h2>${product.productdisplayname}</h2>
+      <p>${product.brandname} / ${product.articletype}</p>
+      <div class="size-row">
+        <label for="size">Choose a size</label>
+        <select id="size">
+          <option>S</option>
+          <option>M</option>
+          <option>L</option>
+          <option>XL</option>
+        </select>
+      </div>
+      <button class="basket-btn">Add to basket</button>
+    </article>`;
+}
